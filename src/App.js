a417534980d708b97eecd6b4cf90f040b98e574f
missFixIt <126031655+missFixIt@users.noreply.github.com>
2023-12-08 11:38:57 -0800

    first commit of clowncracks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import sounds from './sounds_list';
+//import bruce from './audio/Bruce01.mp3';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+  // for (let i = 0; i < sounds.length; i++){
+  //   sounds[i]
+    
+  // };
+  ///a javascript constructor
+ // const audioElement = new Audio("car_horn.wav");
+    //const audio = document.getElementById("audio_tag");
+    const [play, setPlay] = useState(false);
+      const clickHandler = (e) => {
+        setPlay(!play);
+        play ? document.getElementById('clown' + e.target.id).pause() : document.getElementById('clown' + e.target.id).play();
+      
+    };
+    
+  return (
+    <section className='clowns'>
+      <h1>This Week's Clowns</h1>
+      
+        {/* <button
+          onClick={clickHandler}>
+          bruce
+        </button>
+        <audio id="audio_tag" src={bruce} loop/> */}
+    
+      {sounds.map((item) => <div className='clown-item' key={item.id}>
+        <audio id={'clown' +  item.id } src={item.url} loop />
+        <button id={item.id}
+          // onClick={() => {
+          //   let audio=document.getElementById('clown' +  item.id);
+          //   play ? setPlay(false) : setPlay(true);
+          //   play ? audio.pause() : audio.play();
+          // }}
+         onClick={clickHandler}
+          >
+          {item.buttonName}
+        </button>
+      </div>)} 
+      
+    </section>
+    )
+  
+};
 export default App;
